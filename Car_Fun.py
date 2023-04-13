6757c429ad64fa46e6413d20ce82b1c01781d5e5
Beris Dizdar <Berisd2010@gmail.com>
2023-04-13 19:15:32 -0400

    Add new column

--- Car_Fun.py
+++ Car_Fun.py
@@ -3,7 +3,8 @@ import pandas as pd
 mydataset = {'cars':["Audi", "BMW", "Mercedes", "VW", "Ford", "Chevy", "Jaguar", "Honda", "Toyota", "Nissan", "Lexus", "Infiniti", "Acura", "Cadilac"],
              'inventory': [5, 3, 4, 11, 9, 7, 2, 12, 9, 6, 3, 2, 2, 3],
              'color':["Silver", "Grey", "Silver", "Black", "Blue", "Red", "Grey", "Silver", "Green", "Grey", "White", "Black", "Red", "Black"],
-             'origin': ["Germany", "Germany", "Germany", "Germany", "America", "America", "Britain", "Japan", "Japan", "Japan", "Japan", "Japan", "Japan", "America"]}   
+             'origin': ["Germany", "Germany", "Germany", "Germany", "America", "America", "Britain", "Japan", "Japan", "Japan", "Japan", "Japan", "Japan", "America"],
+             'luxury': ["Y", "Y", "Y", "N", "N", "N", "Y", "N", "N", "N", "Y", "Y", "Y", "Y"]}   
 
 myvar = pd.DataFrame(mydataset)
 
@@ -15,6 +16,6 @@ car_origin = myvar['origin']
 car_row = myvar.iloc[7]
 #print(mydataset)
 
-print(car_row)   
+print(myvar)   
 
  
